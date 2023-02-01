f8000795b3a65501410f66cfb80966f721874cd5
Abdullah Ibn Amin <69723627+abdullahibnamin@users.noreply.github.com>
2023-02-01 23:00:25 +0600

    2nd commit

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,5 @@
 from django.views import generic
-
-from product.models import Variant
+from product.models import Variant, Product, ProductVariant
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +10,15 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ctxproducts'] = Product.objects.prefetch_related('productvariant_set')
+
         return context
